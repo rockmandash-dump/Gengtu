baeb075aa4d89f50813e3c95ee9ff863f3b7981e
david44869 <david44869@gmail.com>
2015-05-26 22:44:07 +0800

    抓圖註解

--- app/src/androidTest/java/com/gengtu/gengtu/writetext.java
+++ app/src/androidTest/java/com/gengtu/gengtu/writetext.java
@@ -1,20 +1,19 @@
-
 import java.io.File;
 import java.net.URL;
 import java.lang.String;
 
-
 public class writetext {
 
     public static void main(String[] args) throws Exception {
-        final BufferedImage image = ImageIO.read(new URL(
+        BufferedImage image = ImageIO.read(new URL(
                 "http://upload.wikimedia.org/wikipedia/en/2/24/Lenna.png"));
+        //用BufferedImage來抓取圖檔並存在image， 可用new File(String file)抓檔案
 
-        Graphics g = image.getGraphics();
-        g.setFont(g.getFont().deriveFont(30f));
-        g.drawString("Hello World!", 100, 100);
-        g.dispose();
+        Graphics g = image.getGraphics(); //用getGraphics來對image繪圖
+        g.setFont(g.getFont().deriveFont(30f)); //設定字型
+        g.drawString("Hello World!", 100, 100); //寫下所需要的字串，這裡為Hello World!  並且加上座標X與Y的數值。
+        g.dispose();//釋放系統資源，並且停止繪圖。
 
-        ImageIO.write(image, "png", new File("test.png"));
+        ImageIO.write(image, "jpeg", new File("test.jpg"));//把加字的圖存成test.jpg
     }
 }
